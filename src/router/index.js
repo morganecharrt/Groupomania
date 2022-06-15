e988fe89df3c9d71a6d843cb30d7040087b8b2fb
Morgane Charreteur <morganecharreteur@gmail.com>
2022-06-15 16:05:40 +0200

    Routes application front

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "@/views/HomePage.vue";
+import Login from "@/views/LoginPage.vue";
+import Signup from "@/views/SignupPage.vue";
+import ErrorPage from "@/views/NotFoundPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+    {
+      path: "/create-post",
+      name: "create-post",
+      component: () => import("../views/CreatePostPage.vue"),
+    },
+    {
+      path: "/modify-post/:id",
+      name: "modify-post",
+      component: () => import("../views/ModifyPostPage.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: Signup,
+    },
+
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: ErrorPage,
+    },
+  ],
+});
+
+export default router;
